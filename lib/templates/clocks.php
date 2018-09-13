9b12b2aceca0da76cc53b7c347b38afd8793d596
Christopher Mitchell <chris@designbychris.co.uk>
2018-09-13 13:22:20 +0100

    Shortcode Updates
    Updates for Shortcodes

---
+++ lib/templates/clocks.php
@@ -0,0 +1,15 @@
+<!-- World -->
+<div class="fullwidth bg-light-gray  paddingtop paddingbottom">
+<div class="container counter">
+<h2 class="paddingbottom massivetitle">Looking for one of our other offices?</h2>
+	<div class="row text-center">
+        <div id="clock_americas" onclick="location.href='http://designbychris.co.uk/akvo/newsite/americas.php';"></div>
+		<div id="clock_west_africa" onclick="location.href='http://designbychris.co.uk/akvo/newsite/west-africa.php';"></div>
+		<div id="clock_europe" onclick="location.href='http://designbychris.co.uk/akvo/newsite/europe.php';"></div>
+		<div id="clock_east_africa" onclick="location.href='http://designbychris.co.uk/akvo/newsite/east-africa.php';"></div>
+		<div id="clock_south_asia" onclick="location.href='http://designbychris.co.uk/akvo/newsite/south-asia.php';"></div>
+		<div id="clock_apac" onclick="location.href='http://designbychris.co.uk/akvo/newsite/apac.php';"></div>
+	</div>
+</div>
+</div>
+<!-- End World -->

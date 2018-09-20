620b0a468cf48ed5237e2051c6f262aaa5149385
Christopher Mitchell <chris@designbychris.co.uk>
2018-09-20 12:13:46 +0100

    update to add nested filters shortcode

--- lib/shortcodes.php
+++ lib/shortcodes.php
@@ -1,81 +1,55 @@
 <?php
 	
 	add_shortcode( 'akvo_sample', function( $atts ){
-		
 		ob_start();
-		
 		include('templates/sample.php');
-		
 		return ob_get_clean();
-		
 	} );
 	
 	add_shortcode( 'akvo_clocks', function( $atts ){
-		
 		ob_start();
-		
 		include('templates/clocks.php');
-		
 		return ob_get_clean();
-		
 	} );
 	
 	add_shortcode( 'akvo_logo_slider', function( $atts ){
-		
 		ob_start();
-		
 		include('templates/logo_slider.php');
-		
 		return ob_get_clean();
-		
 	} );
 	
 	add_shortcode( 'akvo_button', function( $atts ){
-		
 		ob_start();
-		
 		include('templates/button.php');
-		
 		return ob_get_clean();
-		
 	} );
 	
 	add_shortcode( 'akvo_modal_button', function( $atts ){
-		
 		ob_start();
-		
 		include('templates/button_modal.php');
-		
 		return ob_get_clean();
-		
 	} );
 	
 	add_shortcode( 'akvo_multistage_form', function( $atts ){
-		
 		ob_start();
-		
 		include('templates/multistage_form.php');
-		
 		return ob_get_clean();
-		
 	} );
 	
 	add_shortcode( 'akvo_map_tab', function( $atts ){
-		
 		ob_start();
-		
 		include('templates/map_tab.php');
-		
 		return ob_get_clean();
-		
 	} );
 	
 	add_shortcode( 'akvo_sector_stories', function( $atts ){
-		
 		ob_start();
-		
 		include('templates/sector_stories.php');
-		
 		return ob_get_clean();
-		
+	} );
+	
+	add_shortcode( 'akvo_filter_stories', function( $atts ){
+		ob_start();
+		include('templates/filter_stories.php');
+		return ob_get_clean();
 	} );

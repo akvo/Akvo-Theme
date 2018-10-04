68db0c2cfab4a68156a0dffee1dcf4845b7f36c6
Christopher Mitchell <chris@designbychris.co.uk>
2018-10-04 09:39:46 +0100

    Update shortcodes.php

--- lib/shortcodes.php
+++ lib/shortcodes.php
@@ -53,6 +53,12 @@
 		include('templates/filter_stories.php');
 		return ob_get_clean();
 	} );
+
+	add_shortcode( 'akvo_statistics', function( $atts ){
+		ob_start();
+		include('templates/statistics_module.php');
+		return ob_get_clean();
+	} );
 	
 	
 	

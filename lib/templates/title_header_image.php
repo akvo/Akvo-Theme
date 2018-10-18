5ccbe848f7002ec6588adfb3fab2cd469d4d40bc
Christopher Mitchell <chris@designbychris.co.uk>
2018-10-18 11:28:35 +0100

    Carousel Fade test

--- lib/templates/title_header_image.php
+++ lib/templates/title_header_image.php
@@ -2,7 +2,7 @@
 <div class="container fullwidth short-title">
     <div id="myCarousel" class="carousel slide" data-ride="carousel">
 		<!-- Wrapper for slides -->
-		<div class="carousel-inner">
+		<div class="carousel-inner carousel-fade">
 			<div class="item active" style="background:url('<?php _e( $atts['bg_image'] );?>') repeat;">
 				<div class="container">
 					<div class="carousel-caption microstory"><h1><?php _e( $atts['title'] );?></h1></div>

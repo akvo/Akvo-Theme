e65d02b143d0d85c58ef315a5a9f0530dfc9aa4c
Christopher Mitchell <chris@designbychris.co.uk>
2018-10-31 10:06:48 +0000

    Change to Hello text on clocks

--- lib/templates/clocks.php
+++ lib/templates/clocks.php
@@ -3,12 +3,12 @@
 	<div class="container counter">
 		<h2 class="paddingbottom massivetitle"><?php _e( $atts['title'] );?></h2>
 		<div class="row text-center">
-			<?php echo do_shortcode("[akvo_clock id='clock_americas' offset='-5.0' title='Americas' hello='Hi There' link='']");?>
-			<?php echo do_shortcode("[akvo_clock id='clock_west_africa' offset='+0' title='West Africa' hello='Bonjour' link='']");?>
-			<?php echo do_shortcode("[akvo_clock id='clock_europe' offset='+2' title='Europe' hello='Hallo' link='']");?>
-			<?php echo do_shortcode("[akvo_clock id='clock_east_africa' offset='+3' title='East Africa' hello='Hujambo' link='']");?>
+			<?php echo do_shortcode("[akvo_clock id='clock_americas' offset='-5.0' title='Americas' hello='Hola' link='']");?>
+			<?php echo do_shortcode("[akvo_clock id='clock_west_africa' offset='+0' title='West Africa' hello='Salut' link='']");?>
+			<?php echo do_shortcode("[akvo_clock id='clock_europe' offset='+2' title='Europe' hello='Hello' link='']");?>
+			<?php echo do_shortcode("[akvo_clock id='clock_east_africa' offset='+3' title='East Africa' hello='Jambo' link='']");?>
 			<?php echo do_shortcode("[akvo_clock id='clock_south_asia' offset='+5' title='South Asia' hello='Namaste' link='']");?>
-			<?php echo do_shortcode("[akvo_clock id='clock_apac' offset='+7' title='South East Asia & Pacific' hello='Ni Hao' link='']");?>
+			<?php echo do_shortcode("[akvo_clock id='clock_apac' offset='+7' title='South East Asia & Pacific' hello='Apu kabar' link='']");?>
 		</div>
 	</div>
 </div>

11589f2c0ce299c520bc3431f2e1142de68f2b4a
Christopher Mitchell <chris@designbychris.co.uk>
2018-11-01 15:02:52 +0000

    update to map_tab.php

--- lib/templates/map_tab.php
+++ lib/templates/map_tab.php
@@ -1,37 +1,33 @@
 <!-- Map -->
-<div class="fullwidth paddingtop paddingbottom">
-	<div class="container">
-		<h3 class="thin paddingbottom largetitle">Drop by for a coffee and a chat at one of our Europe Offices.</h3>
-		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-8">
-			<div class="bhoechie-tab">
-				<div class="bhoechie-tab-content active">
-					<iframe src="https://snazzymaps.com/embed/88606" width="100%" height="300px" style="border:none;"></iframe>
-				</div>
-				<div class="bhoechie-tab-content">
-					<iframe src="https://snazzymaps.com/embed/88606" width="100%" height="300px" style="border:none;"></iframe>
-				</div>
-			</div>
+<div class="fullwidth paddingtop paddingbottom" id="maps">
+<div class="container">
+
+<h2 class="paddingbottom">Drop by for a coffee and a chat at one of our Europe Offices.</h2>
+
+<div class="row row-eq-height">
+        <div class="col col-lg-9 col-md-9 col-xs-12">
+          <!-- Tab panes -->
+          <div class="tab-content">
+            <div class="tab-pane active" id="address"><iframe src="https://snazzymaps.com/embed/88606" width="100%" height="300px" style="border:none;"></iframe></div>
+            <div class="tab-pane" id="address-1"><iframe src="https://snazzymaps.com/embed/88606" width="100%" height="300px" style="border:none;"></iframe></div>
+          </div>
+        </div>
+
+        <div class="col col-lg-3 col-md-3 col-xs-12"> <!-- required for floating -->
+          <!-- Nav tabs -->
+          <ul class="nav nav-tabs tabs-right sideways">
+            <li class="active"><a href="#address" data-toggle="tab"><h3>Other Address in Amsterdam</h3>
+<p>‘s-Gravenhekje 1A<br>
+1011 TG Amsterdam<br>
+The Netherlands</p></a></li>
+            <li><a href="#address-1" data-toggle="tab"><h3>Other Address in Amsterdam</h3>
+<p>‘s-Gravenhekje 1A<br>
+1011 TG Amsterdam<br>
+The Netherlands</p></a></li>
+          </ul>
+        </div>
 		</div>
-		<div class="bhoechie-tab-menu">
-			<div class="list-group">
-				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-4 addressbox first active">
-					<a href="" class="active text-center">
-						<h3>Address in Amsterdam</h3>
-						<p>‘s-Gravenhekje 1A<br>
-						1011 TG Amsterdam<br>
-						The Netherlands</p>
-					</a>
-				</div>
-				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-4 addressbox">
-					<a href="" class="text-center">
-						<h3>Other Address in Amsterdam</h3>
-						<p>‘s-Gravenhekje 1A<br>
-						1011 TG Amsterdam<br>
-						The Netherlands</p>
-					</a>
-				</div>
-			</div>
-		</div>
-	</div>
+
+</div>
 </div>
 <!-- End Map -->

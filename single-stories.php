00b5729cbbf3c8d6a1c310fa6201c35998108bd9
Christopher Mitchell <chris@designbychris.co.uk>
2018-11-12 16:26:08 +0000

    Add files via upload

---
+++ single-stories.php
@@ -0,0 +1,34 @@
+<?php include 'header.php';?>
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<!-- Carousel -->
+<div class="container fullwidth post">
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    
+      <!-- Wrapper for slides -->
+      <div class="carousel-inner">
+		<?php if ( get_field( 'background_header_image' ) ): ?>
+		<div class="item active" style="background:url(<?php the_field('background_header_image'); ?>)!important;background-size:auto!important;background-repeat:repeat!important;">
+		<?php else: ?>
+		<div class="item active" style="background:url(<?php echo esc_url( home_url( '/' ) ); ?>/wp-content/uploads/2018/10/BP-pink-3-600-1.jpg)!important;background-size:auto!important;background-repeat:repeat!important;">
+		<?php endif; ?>
+		<div class="container">
+           <div class="carousel-caption microstory">
+            <h1><?php the_title(); ?></h1>
+			
+          </div>
+		</div>
+        </div><!-- End Item -->
+                        
+      </div><!-- End Carousel Inner -->
+
+    </div>
+</div>
+<!-- End Carousel -->
+
+<!-- Title -->
+<div class="container paddingtop paddingbottom">
+<?php the_content();?>
+</div>
+<!-- End Title -->
+
+<?php include 'footer.php';?>

7ca4848555d5b9a47dee35ab593c8a17efb2b43c
Samuel Thomas <samvthom16@gmail.com>
2018-11-15 14:36:57 +0530

    [#167] - update text for SEAP

--- lib/templates/clocks.php
+++ lib/templates/clocks.php
@@ -8,7 +8,7 @@
 			<?php echo do_shortcode("[akvo_clock id='clock_europe' offset='+2' title='Europe' hello='Hello' link='']");?>
 			<?php echo do_shortcode("[akvo_clock id='clock_east_africa' offset='+3' title='East Africa' hello='Jambo' link='']");?>
 			<?php echo do_shortcode("[akvo_clock id='clock_south_asia' offset='+5' title='South Asia' hello='Namaste' link='']");?>
-			<?php echo do_shortcode("[akvo_clock id='clock_apac' offset='+7' title='South East Asia & Pacific' hello='Apu kabar' link='']");?>
+			<?php echo do_shortcode("[akvo_clock id='clock_apac' offset='+7' title='South East Asia & Pacific' hello='Apa kabar?' link='']");?>
 		</div>
 	</div>
 </div>

7a16a6118f3cf557076414086281e92b267f8f81
Christopher Mitchell <chris@designbychris.co.uk>
2018-11-15 10:24:34 +0000

    Update category.php

--- category.php
+++ category.php
@@ -22,25 +22,9 @@
 
 <!-- Posts -->
 <div class="container paddingtop paddingbottom">
-<div class="row row-eq-height paddingbottom">
-<?php
-        $current_page = get_queried_object();
-        $category     = $current_page->post_name;
-
-        $paged = get_query_var( 'paged' ) ? get_query_var( 'paged' ) : 1;
-        $query = new WP_Query( 
-            array(
-                'paged'         => $paged, 
-                'category_name' => $category,
-                'order'         => 'DESC',
-                'post_type'     => 'post',
-                'post_status'   => 'publish',
-            )
-        );
-
-        if ($query->have_posts()) {
-               while ($query->have_posts()) { 
-               $query->the_post(); ?>
+<?php if ( have_posts() ) :  ?>
+		   <div class="row row-eq-height paddingbottom">
+		   <?php while ( have_posts() ) : the_post(); ?>
 				<div class="col col-lg-4 col-md-4 col-sm-4 col-xs-12 postpaddingbottom">
                     <div class="col-lg-12 col-xs-12">
                         <a href="<?php the_permalink(); ?>"><div class="featuredimage blogimagesmall" style="background:url(<?php the_post_thumbnail_url('full'); ?>);"></div></a>
@@ -54,19 +38,17 @@
 						</div>
 					</div>
                 </div>
-		   <?php
-            }
-
-            // next_posts_link() usage with max_num_pages
-            next_posts_link( 'Older Entries', $query->max_num_pages );
-            previous_posts_link( 'Newer Entries' );
-
-            wp_reset_postdata();
-        }
-        ?>
+				<?php endwhile;?>
 			</div>
 
-
+<?php wp_reset_postdata(); ?>
+<?php endif; ?>
+<div class="row paddingbottom">
+<div class="pagenav">
+    <div class="alignleft"><?php previous_posts_link('Newer Posts', $query->max_num_pages) ?></div>
+    <div class="alignright"><?php next_posts_link('Older Posts', $query->max_num_pages) ?></div>
+</div>
+</div>
 
 </div>
 <!-- End Posts -->

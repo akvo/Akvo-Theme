395d293d365984ba0d31278cd2aec147c274792c
Christopher Mitchell <chris@designbychris.co.uk>
2018-11-20 12:39:07 +0000

    Add files via upload

---
+++ lib/templates/akvo_iframe_report.php
@@ -0,0 +1,8 @@
+<!-- Akvo Report -->
+
+<div class="container paddingtop paddingbottom aligncenter">
+<h2 class="paddingbottom">Akvo application for UN Women in the Pacific</h2>
+<iframe src="https://rsr.test.akvo.org/en/project/5262/?iframe" width="100%" height="100%" align="center" id="toolframe"></iframe>
+</div>
+
+<!-- End Akvo Report -->

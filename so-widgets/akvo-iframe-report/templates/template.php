138a5c7667b3bba04946e6859a24620968adc2bb
Christopher Mitchell <chris@designbychris.co.uk>
2018-12-03 14:36:54 +0000

    Update template.php

--- so-widgets/akvo-iframe-report/templates/template.php
+++ so-widgets/akvo-iframe-report/templates/template.php
@@ -1,6 +1,6 @@
 <!-- Akvo Report -->
 <div class="so-widget-sow-editor so-widget-sow-editor-base">
-<h2 class="paddingbottom"><?php _e( $instance['title'] );?></h2>
+<h3 class="widget-title"><?php _e( $instance['title'] );?></h3>
 <iframe src="<?php _e( $instance['src'] );?>?iframe" width="100%" height="100%" align="center" id="toolframe"></iframe>
 </div>
 <!-- End Akvo Report -->

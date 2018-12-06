c4fc3f79b7c81e9d387fbb42876bfe428ee2c5cc
Christopher Mitchell <chris@designbychris.co.uk>
2018-12-06 16:01:46 +0000

    Tweet pull out box

---
+++ lib/templates/akvo_tweet.php
@@ -0,0 +1,8 @@
+<!-- TWEET PULL OUT -->
+<div class="tweetboxpullout">
+<div class="greenbg greybox fullheight">
+<h3>Having never used any of the technologies in the Akvo Flow stack, I was grateful for the time I was given to learn and explore on my own before starting actual work on Flow.” #Akvo #internship</h3>
+<h3 class="tweetbutton"><a href="#" target="_blank">Click to tweet <i class="fa fa-twitter" aria-hidden="true"></i></a></h3>
+</div>
+</div>
+<!-- END TWEET PULL OUT -->

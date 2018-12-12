22cc247ba0b2a2f0eb02ce573edffeae67d1abc8
Samuel Thomas <samvthom16@gmail.com>
2018-12-12 20:44:42 +0530

    [#5] - refactor single post

--- lib/templates/akvo_accordion.php
+++ lib/templates/akvo_accordion.php
@@ -1,85 +1,84 @@
 <!-- AKVO ACCORDION -->
 <div class="accordion" id="accordionJobs">
-						<div class="card">
-							<div class="card-header" id="headingOne">
-								<h5 class="mb-0">
-							<button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-							 Copywriter - Europe <i class="fas fa-arrow-circle-down arrowdown"></i>
-							</button>
-						  </h5>
-							</div>
+	<div class="card">
+		<div class="card-header" id="headingOne">
+			<h5 class="mb-0">
+				<button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+					Copywriter - Europe <i class="fas fa-arrow-circle-down arrowdown"></i>
+				</button>
+			</h5>
+		</div>
+		
+		<div id="collapseOne" class="collapse fade" aria-labelledby="headingOne" data-parent="#accordionJobs">
+			<div class="card-body">
+				<p><strong>Position:</strong> Copywriter</p>
+				<p><strong>Type:</strong> Permanent</p>
+				<p><strong>Location:</strong> <a href="https://designbychris.co.uk/akvo/newsite/europe.html">Amsterdam</a></p>
+				<p><strong>Department:</strong> Social Media</p>
+				<p><strong>Salary:</strong> TBD</p>
+				<p><strong>Start Date:</strong> ASAP</p>
+				<p><strong>Job Description</strong></p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur mauris elit, commodo non lacinia eget, interdum eu ex. Morbi sed auctor dolor. Duis gravida viverra leo, rutrum gravida mauris congue eu. Donec sit amet felis nisl. Donec at velit in magna ultrices sodales id et leo. Quisque sollicitudin finibus libero, quis mollis libero porttitor sed. Fusce commodo, metus quis vulputate ultrices, lacus odio pharetra purus, non iaculis turpis nunc at leo. Duis convallis semper elementum. Curabitur dignissim nisi at massa lobortis eleifend. Suspendisse sit amet venenatis orci, in fermentum arcu. Sed pharetra dolor a augue tempus, ac ullamcorper lacus pharetra. Fusce eget sodales purus, ac volutpat diam.</p>
+				<p>Proin id risus vel tellus iaculis scelerisque vel et ligula. Morbi a nunc felis. Nulla facilisi. Phasellus sit amet facilisis leo. Nullam eu lorem dolor. Morbi in urna magna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce eu ligula eget eros vehicula elementum ac at tellus. Integer iaculis lorem est, sed fermentum est porttitor ut. In pretium quam lobortis hendrerit porttitor. Aliquam condimentum imperdiet scelerisque.</p>
+				<br/>
+				<p><button data-toggle="modal" data-target="#squarespaceModalApply" class="buttonblack">Apply Now</button></p>
+				<br/>
+			</div>
+		</div>
+	</div>
+	<div class="card">
+		<div class="card-header" id="headingTwo">
+			<h5 class="mb-0">
+				<button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+					C# Programmer - APAC <i class="fas fa-arrow-circle-down arrowdown"></i>
+				</button>
+			</h5>
+		</div>
 
-							<div id="collapseOne" class="collapse fade" aria-labelledby="headingOne" data-parent="#accordionJobs">
-								<div class="card-body">
-								<p><strong>Position:</strong> Copywriter</p>
-								<p><strong>Type:</strong> Permanent</p>
-								<p><strong>Location:</strong> <a href="https://designbychris.co.uk/akvo/newsite/europe.html">Amsterdam</a></p>
-								<p><strong>Department:</strong> Social Media</p>
-								<p><strong>Salary:</strong> TBD</p>
-								<p><strong>Start Date:</strong> ASAP</p>
-								<p><strong>Job Description</strong></p>
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur mauris elit, commodo non lacinia eget, interdum eu ex. Morbi sed auctor dolor. Duis gravida viverra leo, rutrum gravida mauris congue eu. Donec sit amet felis nisl. Donec at velit in magna ultrices sodales id et leo. Quisque sollicitudin finibus libero, quis mollis libero porttitor sed. Fusce commodo, metus quis vulputate ultrices, lacus odio pharetra purus, non iaculis turpis nunc at leo. Duis convallis semper elementum. Curabitur dignissim nisi at massa lobortis eleifend. Suspendisse sit amet venenatis orci, in fermentum arcu. Sed pharetra dolor a augue tempus, ac ullamcorper lacus pharetra. Fusce eget sodales purus, ac volutpat diam.</p>
-								<p>Proin id risus vel tellus iaculis scelerisque vel et ligula. Morbi a nunc felis. Nulla facilisi. Phasellus sit amet facilisis leo. Nullam eu lorem dolor. Morbi in urna magna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce eu ligula eget eros vehicula elementum ac at tellus. Integer iaculis lorem est, sed fermentum est porttitor ut. In pretium quam lobortis hendrerit porttitor. Aliquam condimentum imperdiet scelerisque.</p>
-								<br/>
-								<p><button data-toggle="modal" data-target="#squarespaceModalApply" class="buttonblack">Apply Now</button></p>
-								<br/>
-								</div>
-							</div>
-						</div>
+		<div id="collapseTwo" class="collapse fade" aria-labelledby="headingTwo" data-parent="#accordionJobs">
+			<div class="card-body">
+				<p><strong>Position:</strong> C# Programmer</p>
+				<p><strong>Type:</strong> Temporary/Contract</p>
+				<p><strong>Location:</strong> <a href="#">Singapore</a></p>
+				<p><strong>Department:</strong> Product Development</p>
+				<p><strong>Salary:</strong> TBD</p>
+				<p><strong>Start Date:</strong> ASAP</p>
+				<p><strong>Job Description</strong></p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur mauris elit, commodo non lacinia eget, interdum eu ex. Morbi sed auctor dolor. Duis gravida viverra leo, rutrum gravida mauris congue eu. Donec sit amet felis nisl. Donec at velit in magna ultrices sodales id et leo. Quisque sollicitudin finibus libero, quis mollis libero porttitor sed. Fusce commodo, metus quis vulputate ultrices, lacus odio pharetra purus, non iaculis turpis nunc at leo. Duis convallis semper elementum. Curabitur dignissim nisi at massa lobortis eleifend. Suspendisse sit amet venenatis orci, in fermentum arcu. Sed pharetra dolor a augue tempus, ac ullamcorper lacus pharetra. Fusce eget sodales purus, ac volutpat diam.</p>
+				<p>Proin id risus vel tellus iaculis scelerisque vel et ligula. Morbi a nunc felis. Nulla facilisi. Phasellus sit amet facilisis leo. Nullam eu lorem dolor. Morbi in urna magna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce eu ligula eget eros vehicula elementum ac at tellus. Integer iaculis lorem est, sed fermentum est porttitor ut. In pretium quam lobortis hendrerit porttitor. Aliquam condimentum imperdiet scelerisque.</p>
+				<br/>
+				<p><button data-toggle="modal" data-target="#squarespaceModalApply" class="buttonblack">Apply Now</button></p>
+				<br/>
+			</div>
+		</div>
+	</div>
 						
-						<div class="card">
-							<div class="card-header" id="headingTwo">
-								<h5 class="mb-0">
-							<button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-							 C# Programmer - APAC <i class="fas fa-arrow-circle-down arrowdown"></i>
-							</button>
-						  </h5>
-							</div>
+	<div class="card">
+		<div class="card-header" id="headingThree">
+			<h5 class="mb-0">
+				<button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+					Marketing Executive - West Africa <i class="fas fa-arrow-circle-down arrowdown"></i>
+				</button>
+			</h5>
+		</div>
 
-							<div id="collapseTwo" class="collapse fade" aria-labelledby="headingTwo" data-parent="#accordionJobs">
-								<div class="card-body">
-								<p><strong>Position:</strong> C# Programmer</p>
-								<p><strong>Type:</strong> Temporary/Contract</p>
-								<p><strong>Location:</strong> <a href="#">Singapore</a></p>
-								<p><strong>Department:</strong> Product Development</p>
-								<p><strong>Salary:</strong> TBD</p>
-								<p><strong>Start Date:</strong> ASAP</p>
-								<p><strong>Job Description</strong></p>
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur mauris elit, commodo non lacinia eget, interdum eu ex. Morbi sed auctor dolor. Duis gravida viverra leo, rutrum gravida mauris congue eu. Donec sit amet felis nisl. Donec at velit in magna ultrices sodales id et leo. Quisque sollicitudin finibus libero, quis mollis libero porttitor sed. Fusce commodo, metus quis vulputate ultrices, lacus odio pharetra purus, non iaculis turpis nunc at leo. Duis convallis semper elementum. Curabitur dignissim nisi at massa lobortis eleifend. Suspendisse sit amet venenatis orci, in fermentum arcu. Sed pharetra dolor a augue tempus, ac ullamcorper lacus pharetra. Fusce eget sodales purus, ac volutpat diam.</p>
-								<p>Proin id risus vel tellus iaculis scelerisque vel et ligula. Morbi a nunc felis. Nulla facilisi. Phasellus sit amet facilisis leo. Nullam eu lorem dolor. Morbi in urna magna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce eu ligula eget eros vehicula elementum ac at tellus. Integer iaculis lorem est, sed fermentum est porttitor ut. In pretium quam lobortis hendrerit porttitor. Aliquam condimentum imperdiet scelerisque.</p>
-								<br/>
-								<p><button data-toggle="modal" data-target="#squarespaceModalApply" class="buttonblack">Apply Now</button></p>
-								<br/>
-								</div>
-							</div>
-						</div>
-						
-						<div class="card">
-							<div class="card-header" id="headingThree">
-								<h5 class="mb-0">
-							<button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-							 Marketing Executive - West Africa <i class="fas fa-arrow-circle-down arrowdown"></i>
-							</button>
-						  </h5>
-							</div>
-
-							<div id="collapseThree" class="collapse fade" aria-labelledby="headingThree" data-parent="#accordionJobs">
-								<div class="card-body">
-								<p><strong>Position:</strong> Marketing Executive</p>
-								<p><strong>Type:</strong> Contract (6 months)</p>
-								<p><strong>Location:</strong> <a href="#">Nigeria</a></p>
-								<p><strong>Department:</strong> Marketing</p>
-								<p><strong>Salary:</strong> TBD</p>
-								<p><strong>Start Date:</strong> ASAP</p>
-								<p><strong>Job Description</strong></p>
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur mauris elit, commodo non lacinia eget, interdum eu ex. Morbi sed auctor dolor. Duis gravida viverra leo, rutrum gravida mauris congue eu. Donec sit amet felis nisl. Donec at velit in magna ultrices sodales id et leo. Quisque sollicitudin finibus libero, quis mollis libero porttitor sed. Fusce commodo, metus quis vulputate ultrices, lacus odio pharetra purus, non iaculis turpis nunc at leo. Duis convallis semper elementum. Curabitur dignissim nisi at massa lobortis eleifend. Suspendisse sit amet venenatis orci, in fermentum arcu. Sed pharetra dolor a augue tempus, ac ullamcorper lacus pharetra. Fusce eget sodales purus, ac volutpat diam.</p>
-								<p>Proin id risus vel tellus iaculis scelerisque vel et ligula. Morbi a nunc felis. Nulla facilisi. Phasellus sit amet facilisis leo. Nullam eu lorem dolor. Morbi in urna magna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce eu ligula eget eros vehicula elementum ac at tellus. Integer iaculis lorem est, sed fermentum est porttitor ut. In pretium quam lobortis hendrerit porttitor. Aliquam condimentum imperdiet scelerisque.</p>
-								<br/>
-								<p><button data-toggle="modal" data-target="#squarespaceModalApply" class="buttonblack">Apply Now</button></p>
-								<br/>
-								</div>
-							</div>
-						</div>
+		<div id="collapseThree" class="collapse fade" aria-labelledby="headingThree" data-parent="#accordionJobs">
+			<div class="card-body">
+				<p><strong>Position:</strong> Marketing Executive</p>
+				<p><strong>Type:</strong> Contract (6 months)</p>
+				<p><strong>Location:</strong> <a href="#">Nigeria</a></p>
+				<p><strong>Department:</strong> Marketing</p>
+				<p><strong>Salary:</strong> TBD</p>
+				<p><strong>Start Date:</strong> ASAP</p>
+				<p><strong>Job Description</strong></p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur mauris elit, commodo non lacinia eget, interdum eu ex. Morbi sed auctor dolor. Duis gravida viverra leo, rutrum gravida mauris congue eu. Donec sit amet felis nisl. Donec at velit in magna ultrices sodales id et leo. Quisque sollicitudin finibus libero, quis mollis libero porttitor sed. Fusce commodo, metus quis vulputate ultrices, lacus odio pharetra purus, non iaculis turpis nunc at leo. Duis convallis semper elementum. Curabitur dignissim nisi at massa lobortis eleifend. Suspendisse sit amet venenatis orci, in fermentum arcu. Sed pharetra dolor a augue tempus, ac ullamcorper lacus pharetra. Fusce eget sodales purus, ac volutpat diam.</p>
+				<p>Proin id risus vel tellus iaculis scelerisque vel et ligula. Morbi a nunc felis. Nulla facilisi. Phasellus sit amet facilisis leo. Nullam eu lorem dolor. Morbi in urna magna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce eu ligula eget eros vehicula elementum ac at tellus. Integer iaculis lorem est, sed fermentum est porttitor ut. In pretium quam lobortis hendrerit porttitor. Aliquam condimentum imperdiet scelerisque.</p>
+				<br/>
+				<p><button data-toggle="modal" data-target="#squarespaceModalApply" class="buttonblack">Apply Now</button></p>
+				<br/>
+			</div>
+		</div>
+	</div>
 
 </div>
 <!-- END AKVO ACCORDION -->

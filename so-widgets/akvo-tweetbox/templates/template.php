9ef358b22c1ae0410d502ad7873da639dfe52704
Christopher Mitchell <chris@designbychris.co.uk>
2018-12-13 11:00:36 +0000

    Update template.php

--- so-widgets/akvo-tweetbox/templates/template.php
+++ so-widgets/akvo-tweetbox/templates/template.php
@@ -4,10 +4,10 @@
 	$twitter_link = "https://twitter.com/intent/tweet?text=".$instance['text']."&url=".get_the_permalink();
 
 ?>
-<div class="tweetboxpullout">
+<a target="_blank" href="<?php _e( $twitter_link );?>" target="_blank"><div class="tweetboxpullout">
 	<div class="fullheight">
 		<h3><?php _e( $instance['text'] );?></h3>
 		<h3 class="tweetbutton"><a target="_blank" href="<?php _e( $twitter_link );?>" target="_blank">Click to Tweet <i class="fa fa-twitter" aria-hidden="true"></i></a></h3>
 	</div>
-</div>
+</div></a>
 <!-- END TWEET PULL OUT -->

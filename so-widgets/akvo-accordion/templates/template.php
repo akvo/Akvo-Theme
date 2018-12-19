4d5ec8a66ec25f4d1d87f3fdbe213972b94a3250
Christopher Mitchell <chris@designbychris.co.uk>
2018-12-19 12:04:22 +0000

    Update template.php

--- so-widgets/akvo-accordion/templates/template.php
+++ so-widgets/akvo-accordion/templates/template.php
@@ -1,7 +1,7 @@
 <!-- AKVO ACCORDION -->
 <div class="accordion" id="accordionJobs">
 	<?php $i = 0;foreach( $instance['items'] as $item ): $id = "collapse".$i;?>
-	<div class="card">
+	<div class="card" data-parent="#accordionJobs">
 		<div class="card-header" id="heading<?php _e( $i );?>">
 			<h5 class="mb-0">
 				<button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#<?php _e( $id );?>" aria-expanded="false" aria-controls="collapseOne">
@@ -9,7 +9,7 @@
 				</button>
 			</h5>
 		</div>
-		<div id="<?php _e( $id );?>" class="collapse fade" aria-labelledby="heading<?php _e( $i );?>" data-parent="#accordionJobs">
+		<div id="<?php _e( $id );?>" class="collapse fade" aria-labelledby="heading<?php _e( $i );?>" >
 			<div class="card-body">
 				<?php echo siteorigin_panels_render( 'accordion-desc-'.$i, true, $item['desc'] );?>
 			</div>

9a6410fde317b2b8733dd2379181bce2102bfd2e
Samuel Thomas <samvthom16@gmail.com>
2019-02-12 12:52:48 +0530

    [#301] - 404 page

--- 404.php
+++ 404.php
@@ -1,11 +1,17 @@
-<?php /* Template Name: 404 Page */ ?>
 <?php get_header();?>
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<div class="fullwidth homeslider paddingbottom"><?php get_template_part('partials/content', 'header'); ?></div>
-<div class="container paddingtop paddingbottom">
-<div class="container-fluid"><?php the_content();?></div>
+<div class="fullwidth homeslider paddingbottom">
+	<?php get_template_part('partials/content', 'header'); ?></div>
+	<div class="container paddingtop paddingbottom">
+		<div class="container-fluid">
+			<h2><?php esc_html_e( "Sorry, no posts matched your criteria." ); ?></h2>
+			<p>&nbsp;</p><p>&nbsp;</p>
+			<h4 class="thin"><?php esc_html_e( "Or isn't this what you were hoping for?" ); ?></h4>
+			<p>&nbsp;</p>
+			<h4 class="thin"><?php esc_html_e( "In that case something's gone wrong." ); ?></h4>
+			<p>&nbsp;</p>
+			<h4 class="thin"><?php esc_html_e( "You might want to try our home page, or the menu bar above." ); ?></h4>
+			<p>&nbsp;</p><p>&nbsp;</p>
+		</div>
+	</div>
 </div>
-<?php endwhile; else : ?>
-	<p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
-<?php endif; ?>
 <?php get_footer();?>

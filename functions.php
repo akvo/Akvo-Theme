95f2e300e641c8f820c195118d5bfbf067e3d4a2
Samuel Thomas <samvthom16@gmail.com>
2019-02-14 09:02:07 +0530

    [#5] - CSS updates on links

--- functions.php
+++ functions.php
@@ -95,7 +95,7 @@
 		//Enqueue Styles
 		wp_enqueue_style('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css', null, true);
 		wp_enqueue_style('style', get_template_directory_uri() .'/css/main.css', array('bootstrap'), '1.0.0' );
-		wp_enqueue_style('style-chris', get_template_directory_uri() .'/css/chris.css', array('bootstrap'), '1.0.3' );
+		wp_enqueue_style('style-chris', get_template_directory_uri() .'/css/chris.css', array('bootstrap'), '1.0.4' );
 		
 		
 		// load google fonts - can be combined for later

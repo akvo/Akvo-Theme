90ceac37e25eb9c63568218ca7cfec48fb3a21cc
Christopher Mitchell <chris@designbychris.co.uk>
2019-03-06 15:00:30 +0000

    Update akvo_scripts.js

--- js/akvo_scripts.js
+++ js/akvo_scripts.js
@@ -181,7 +181,7 @@ $(document).ready(function(){
 				settings	: { slidesToShow: 4 }
 			}, {
 				breakpoint: 520,
-				settings: { slidesToShow: 3 }
+				settings: { slidesToShow: 2 }
 			}]
 		});
 		

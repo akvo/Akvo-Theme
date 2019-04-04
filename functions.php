c86b32b363e03ce6767b2055622e17e9f2e9ae05
unknown <samvthom16@gmail.com>
2019-04-04 09:16:49 +0530

    #5 - update styles for footer gform

--- functions.php
+++ functions.php
@@ -101,7 +101,7 @@
 		//Enqueue Styles
 		wp_enqueue_style('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css', null, true);
 		wp_enqueue_style('style', get_template_directory_uri() .'/assets/main.css', array('bootstrap'), '1.0.0' );
-		wp_enqueue_style('style-chris', get_template_directory_uri() .'/assets/chris.css', array('bootstrap'), '1.0.4' );
+		wp_enqueue_style('style-chris', get_template_directory_uri() .'/assets/chris.css', array('bootstrap'), '1.0.5' );
 
 
 		// load google fonts - can be combined for later

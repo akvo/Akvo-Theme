fd2fac59627ba89c887ecba712476618380b2741
unknown <samvthom16@gmail.com>
2019-04-18 10:20:17 +0530

    #360 - string translation compatible with WPML

--- lib/templates/clock.php
+++ lib/templates/clock.php
@@ -1,1 +1,1 @@
-<div id="<?php _e( $atts['id'] );?>" data-behaviour='clock' data-offset='<?php _e( $atts['offset'] );?>' data-title="<?php _e( $atts['title'] );?>" data-hello="<?php _e( $atts['hello'] );?>" onclick="location.href='<?php _e( $atts['link'] );?>';"></div>
+<div id="<?php _e( $atts['id'] );?>" data-behaviour='clock' data-offset='<?php _e( $atts['offset'] );?>' data-title="<?php echo __($atts['title'],'akvo'); ?>" data-hello="<?php _e( $atts['hello'] );?>" onclick="location.href='<?php _e( $atts['link'] );?>';"></div>

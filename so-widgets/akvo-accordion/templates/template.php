80af14c0dd52193614eaf5a29492a1bb1c24317a
unknown <samvthom16@gmail.com>
2019-08-30 10:58:13 +0530

    #383 - unique id for each accordion

--- so-widgets/akvo-accordion/templates/template.php
+++ so-widgets/akvo-accordion/templates/template.php
@@ -1,6 +1,22 @@
 <!-- AKVO ACCORDION -->
-<div class="accordion" id="accordionJobs">
-	<?php $i = 0;foreach( $instance['items'] as $item ): $id = "collapse".$i;?>
+<?php
+	// ASSUMING THAT THE AKVO WIDGETS PLUGIN IS ACTIVE
+	global $akvo_widgets_template;
+	if( $akvo_widgets_template ){
+		$widget_id = $akvo_widgets_template->getUniqueID( $instance );
+	}
+	else{
+		$widget_id = "accordionJobs";
+	}
+?>
+<div class="accordion" id="<?php _e( $widget_id );?>">
+	<?php $i = 0;foreach( $instance['items'] as $item ):?>
+	<?php
+		$id = "collapse".$i;
+		if( $akvo_widgets_template ){
+			$id = $akvo_widgets_template->getUniqueID( $item );
+		}
+	?>
 	<div class="card">
 		<div class="card-header" id="heading<?php _e( $i );?>">
 			<h5 class="mb-0">
@@ -9,7 +25,7 @@
 				</button>
 			</h5>
 		</div>
-		<div id="<?php _e( $id );?>" class="collapse fade" aria-labelledby="heading<?php _e( $i );?>" data-parent="#accordionJobs">
+		<div id="<?php _e( $id );?>" class="collapse fade" aria-labelledby="heading<?php _e( $i );?>" data-parent="<?php _e( "#".$widget_id );?>">
 			<div class="card-body">
 				<?php echo siteorigin_panels_render( 'accordion-desc-'.$i, true, $item['desc'] );?>
 			</div>

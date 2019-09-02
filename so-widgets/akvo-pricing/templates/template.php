132e8f48a1686820481a698db300035014456cca
Steve <anil.stephen66@gmail.com>
2019-09-02 12:41:36 +0530

    Updated js and css

--- so-widgets/akvo-pricing/templates/template.php
+++ so-widgets/akvo-pricing/templates/template.php
@@ -1,6 +1,6 @@
 <!-- AKVO CONTENT -->
 <?php global $sp_sow; ?>
-<div class="dropdown-content" style="background-color: <?php _e( $instance['content_backround'] );?>;">
+<div class="sow-pricing" style="background-color: <?php _e( $instance['content_backround'] );?>;">
 		<ul class="list-unstyled">
 	<?php foreach ( $instance['items'] as $value) : $content_id = $sp_sow->getUniqueID( $value ); ?>
 		<li class="faded"><a href="#<?php _e( 'content-'.$content_id ); ?>"><?php _e( $value['content_editor'] );?></a></li>
@@ -11,18 +11,18 @@
 <!-- SHOW CONTENT DIV ON BUTTON CLICK -->
 	<?php global $sp_sow; foreach( $instance['items'] as $value ):
 		if( function_exists( 'siteorigin_panels_render' ) ): $popup_id = $sp_sow->getUniqueID( $value );?>
-	<div class="content-desc" id="content-<?php _e( $popup_id ); ?>">
+	<div class="sow-pricing-desc" id="content-<?php _e( $popup_id ); ?>">
 		<?php echo siteorigin_panels_render( 'w'.$popup_id, true, $value['content_desc'] ) ;?>
 	</div>
 <?php endif;endforeach;?>
 <!-- SHOW CONTENT DIV ON BUTTON CLICK ENDS -->
 <style>
-.dropdown-content ul > li a,
-.dropdown-content ul > li a:hover{
+.sow-pricing ul > li a,
+.sow-pricing ul > li a:hover{
 	color: <?php _e( $instance['content_text'] );?>;
 	text-decoration: none;
 }
-.content-active{
+.sow-desc-active{
 	color: <?php _e( $instance['content_text'] );?>;
 	text-decoration: none;
 }

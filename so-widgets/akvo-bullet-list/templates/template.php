a17905a5dba900c04bd781b47c5bedee1f7a9408
Steve <anil.stephen66@gmail.com>
2019-09-04 16:17:16 +0530

    #385 Updated bullet-list styles

--- so-widgets/akvo-bullet-list/templates/template.php
+++ so-widgets/akvo-bullet-list/templates/template.php
@@ -4,10 +4,14 @@
 		<?php foreach ( $instance['bullet_items'] as $value) :?>
 			<li class="bullet-item row" style="opacity: <?php ( $value['is_faded'] ? _e( '0.5' ) : _e( '1' ) ); ?>;" >
 				<div class="col-md-3">
-					<p class="bullet-title">
-						<i class="<?php _e( $value['bullet_icon_class'] ); ?>" style=";" aria-hidden="true"></i>
-						<strong><?php _e( $value['bullet_title'] );?></strong>
-					</p>
+					<div class="bullet-title">
+						<div class="bullet-title-icon">
+							<i class="<?php _e( $value['bullet_icon_class'] ); ?>" aria-hidden="true"></i>
+						</div>
+						<div class="bullet-title-text">
+							<strong><?php _e( $value['bullet_title'] );?></strong>
+						</div>
+					</div>
 				</div>
 				<div class="col-md-9">
 					<p><?php _e( $value['bullet_desc'] );?></p>

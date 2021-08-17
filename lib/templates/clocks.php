84afe55d8f8eb642005d43cfef4ff8b1b75030bc
psychome004 <anil.stephen66@gmail.com>
2021-08-17 15:47:03 +0530

    Update clocks

--- lib/templates/clocks.php
+++ lib/templates/clocks.php
@@ -6,8 +6,8 @@
 			<?php echo do_shortcode("[akvo_clock id='clock_americas' offset='-4.0' title='Americas' hello='Hola' link='https://akvo.org/americas/']");?>
 			<?php echo do_shortcode("[akvo_clock id='clock_west_africa' offset='+0' title='West Africa' hello='Salut' link='https://akvo.org/west-africa/']");?>
 			<?php echo do_shortcode("[akvo_clock id='clock_europe' offset='+2' title='Europe' hello='Hello' link='https://akvo.org/europe/']");?>
-			<?php echo do_shortcode("[akvo_clock id='clock_east_africa' offset='+3' title='East Africa' hello='Jambo' link='https://akvo.org/east-africa/']");?>
-			<?php echo do_shortcode("[akvo_clock id='clock_apac' offset='+8' title='Asia Pacific' hello='Apa kabar?' link='https://akvo.org/south-east-asia-pacific/']");?>
+			<?php echo do_shortcode("[akvo_clock id='clock_east_africa' offset='+0' title='East Africa' hello='Jambo' link='https://akvo.org/east-africa/']");?>
+			<?php echo do_shortcode("[akvo_clock id='clock_apac' offset='+0' title='Asia Pacific' hello='Apa kabar?' link='https://akvo.org/south-east-asia-pacific/']");?>
 		</div>
 	</div>
 </div>
